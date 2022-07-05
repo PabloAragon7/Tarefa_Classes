510e3f473d7a5101929d36c3155d8f23d01012a9
Pablo Câmara Aragon <pabloaragon7@gmail.com>
2022-07-05 15:26:27 -0300

    exercício mod 7

---
+++ Carro.java
@@ -0,0 +1,70 @@
+/**
+ * @author pablo
+ */
+public class Carro {
+
+    private int codigo;
+
+    private String nome;
+
+    private String tipo;
+
+    private String modelo;
+
+    private int ano;
+
+    private String cor;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public void imprimirModelo() {
+        System.out.println(this.modelo);
+    }
+
+}
+
